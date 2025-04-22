7b20efdd964795ba97e74e5d526af36481579554
nirmalearn <nirmalmangalat.learning@gmail.com>
2025-04-22 23:06:53 +0530

    env changes. iject git srt to crun

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-JWT_SECRET = os.getenv("JWT_SECRET", "your-default-secret")
+JWT_SECRET = os.getenv("JWT_SECRET", "l5-3tKsfg3y_983!hasjsg@xzd")
 MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
 
